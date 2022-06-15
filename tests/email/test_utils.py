3a4fb5a187a0b47a96a64f46d19efbb36971559b
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-06-15 16:04:59 +0200

    add test folder

---
+++ tests/email/test_utils.py
@@ -0,0 +1,15 @@
+#pylint: disable=missing-class-docstring missing-function-docstring
+import unittest
+
+from src.email.utils import extract_emails_from_string
+
+class Test_extract_emails_from_string(unittest.TestCase):
+    
+    def test_extract_one_email(self):
+        self.assertEqual(extract_emails_from_string("I am an email email@domain.com."), ["email@domain.com"])
+        
+    def test_string_empty(self):
+        self.assertEqual(extract_emails_from_string(""), [])
+        
+if __name__ == '__main__':
+    unittest.main()
