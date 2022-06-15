2ee90c09c0068c5529b415343abb299843799359
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-06-15 11:31:03 +0200

    add email service and create documentation

---
+++ src/email/utils.py
@@ -0,0 +1,23 @@
+import re # You may need this
+from typing import List
+
+# Good to take a look at this: https://help.returnpath.com/hc/en-us/articles/220560587-What-are-the-rules-for-email-address-syntax-
+def extract_emails_from_string(string: str) -> List[str]:
+    """Extracts all emails from a string.
+        ## Arguments
+        - `string`: The string to extract emails from.
+        
+        ## Returns
+        - A list of emails if any were found.
+        - An empty list if no emails were found.    
+
+        ## Examples
+        ```
+            emails = extract_emails_from_string("My email is alexandre@domain.com")
+            print(emails) # ["alexandre@domain.com"]
+            
+            no_emails = extract_emails_from_string("This is a string without emails")
+            print(no_emails) # []
+        ```
+    """
+    pass # to remove
