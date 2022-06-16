635ab783aa9ff9e6a335aac6c889216a2bd96c27
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-06-16 15:38:04 +0200

    add unit testing examples for extract_email_from_string

--- tests/email/test_utils.py
+++ tests/email/test_utils.py
@@ -6,10 +6,21 @@ from src.email.utils import extract_emails_from_string
 class Test_extract_emails_from_string(unittest.TestCase):
     
     def test_extract_one_email(self):
-        self.assertEqual(extract_emails_from_string("I am an email email@domain.com."), ["email@domain.com"]) #dsoihfqshgghqdfiufhsdiuosdfhusikdfghsudf
-        
+        self.assertEqual(extract_emails_from_string("I am an email email@domain.com."), ["email@domain.com"]) 
+
     def test_string_empty(self):
         self.assertEqual(extract_emails_from_string(""), [])
+
+    def test_extract_no_email(self):
+        self.assertEqual(extract_emails_from_string("This is not an email: [email@gmsa^il.com]."), [])
+        self.assertEqual(extract_emails_from_string("This is not an email: email@student.tu-delft._com."), [])
+        self.assertEqual(extract_emails_from_string("Not email_email@domain-si"), [])
+
+    def test_extract_special_character_emails(self):
+        self.assertEqual(extract_emails_from_string("Special character emails: <br>email@gmail.com-_<br>."), ["email@gmail.com"])
+        self.assertEqual(extract_emails_from_string("-Az48.29@NEW-domain.Com."), ["Az48.29@NEW-domain.Com"])
+        self.assertEqual(extract_emails_from_string("*Recipient@gmail.com,"), ["Recipient@gmail.com"])
+        self.assertEqual(extract_emails_from_string("re--CI_PIENT@my.outlook.fr."), ["CI_PIENT@my.outlook.fr"])
         
 if __name__ == '__main__':
     unittest.main()
