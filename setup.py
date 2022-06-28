c215e488b493781897455eb59c7f75bf8f4a9ba3
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-06-28 15:20:48 +0200

    add setup file for python package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='src',
+    version='0.0.1',
+    author='Lox Solution',
+    author_email='natasa.zekic@loxsolution.com',
+    description='A package with Lox services',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Lox-Solution/LoxServices',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Lox-Solution/LoxServices/issues"
+    },
+    license='MIT',
+    packages=['src'],
+    install_requires=['importlib.metadata'],
+)
