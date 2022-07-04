38875aa8b2488072df1c58653f800134d6fa4d0e
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-07-04 11:25:15 +0200

    update package version

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+0.0.1 initial package with email service
+0.0.2 translation service added
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='lox_services',
-    version='0.0.1',
+    version='0.0.2',
     author='Lox Solution',
     author_email='natasa.zekic@loxsolution.com',
     description='A package with Lox services',
