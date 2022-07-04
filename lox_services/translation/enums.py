74a22b295eb9c25f4d3be41ea3ae08952417822d
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-07-04 11:00:49 +0200

    add translation service folder and functions

---
+++ lox_services/translation/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class TranslationModules(Enum):
+    """Enumeration of the Lox translation modules."""
+    ROOT = ""
+    BILLING_INVOICE = "billing/invoice"
+    BILLING_EMAIL = "billing/email"
