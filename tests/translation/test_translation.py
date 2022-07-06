b7341c7265b043c76f8797bbe2fdd57be30c1e4b
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-07-06 17:15:15 +0200

    improve translation tests

--- tests/translation/test_translation.py
+++ tests/translation/test_translation.py
@@ -4,34 +4,31 @@ import unittest
 
 from lox_services.translation.translate import get_translations
 from lox_services.translation.templates import insert_variables
+from lox_services.utils import json_file_to_python_dictionary
 
 
-service_path = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "lox_services", "translation")
+service_path = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "lox_services", "translation", "languages")
+json_path_and_length = {'/': 12, '/billing/email/': 11, '/billing/invoice/': 20}
 
 class Test_translation_files(unittest.TestCase):
     
     def test_translation_file_length(self):
         """Checks that number of keys hasn't changed"""
-        file_path_and_length = {'/languages/': 12, '/languages/billing/email/': 11, '/languages/billing/invoice/': 20}
-        for path, length in file_path_and_length.items():
+        for path, length in json_path_and_length.items():
             file_list = [f for f in os.listdir(service_path + path)if f.endswith('.json')]
             for file in file_list:
-                with open(service_path + path + file) as file:
-                    file_data = json.load(file)
-                    assert len(file_data) == length
+                file_data = json_file_to_python_dictionary(service_path + path + file)
+                assert len(file_data) == length
                     
                     
     def test_translation_file_keys(self):
         """Checks that keys match in all languages"""
-        json_file_paths = ['/languages/', '/languages/billing/email/', '/languages/billing/invoice/']
-        for json_path in json_file_paths:
+        for json_path in json_path_and_length:
             json_list = [f for f in os.listdir(service_path + json_path)if f.endswith('.json')]
-            english_json = json.load(open(service_path + json_path + 'EN.json'))
+            english_dictionary = json_file_to_python_dictionary(service_path + json_path + 'EN.json')
             for json_file in json_list:
-                with open(service_path + json_path + json_file) as json_file:
-                    json_data = json.load(json_file)
-                    assert english_json.keys() == json_data.keys()
-                    self.assertEqual(english_json.keys(), json_data.keys())
+                dictionary = json_file_to_python_dictionary(service_path + json_path + json_file)
+                assert english_dictionary.keys() == dictionary.keys()
         
     def test_get_translation_exception(self):
         self.assertRaises(Exception, get_translations, "NL", True)
