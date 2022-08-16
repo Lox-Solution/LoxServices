115e4655c651ba0c87dd9a3327b7acebcc2cd61d
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-08-16 14:07:03 +0200

    add slack service

---
+++ lox_services/slack/send_message.py
@@ -0,0 +1,38 @@
+import json
+import sys
+import requests
+
+from lox_services.config.env_variables import get_env_variable
+
+RUNS_REPORTS="T01DGHEA14Z/B03B6DH43M1/ZVSzEZzkUdJ2cozPz6mRribG"
+RUNS_REPORTS_DEV="T01DGHEA14Z/B03BH1BQA5Q/Zx7afSknm1dLHJaLwyDYQ8Eo"
+
+def send_messages(message: str , title : str):
+    "Send a slack message to run-report channel  "
+    if get_env_variable("ENVIRONMENT") == 'development':
+        url = f"https://hooks.slack.com/services/{RUNS_REPORTS_DEV}"
+    else:
+        url = f"https://hooks.slack.com/services/{RUNS_REPORTS}"    
+    message = message
+    slack_data = {
+        "username": "NotificationBot",
+        "icon_emoji": ":satellite:",
+        #"channel" : "#somerandomcahnnel",
+        "attachments": [
+            {
+                "color": "#F3421C",
+                "fields": [
+                    {
+                        "title": title,
+                        "value": message,
+                        "short": "false",
+                    }
+                ]
+            }
+        ]
+    }
+    byte_length = str(sys.getsizeof(slack_data))
+    headers = {'Content-Type': "application/json", 'Content-Length': byte_length}
+    response = requests.post(url, data=json.dumps(slack_data), headers=headers)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
