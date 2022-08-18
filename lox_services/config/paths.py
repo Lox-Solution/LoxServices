2c3483e40a2d8f45709ef370537db5b28b7ef800
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-08-18 14:24:20 +0200

    reverse external variables

--- lox_services/config/paths.py
+++ lox_services/config/paths.py
@@ -8,8 +8,3 @@ ROOT_PATH: str = getenv("PYTHONPATH")
 # Folder where results will be stored. These folders are ignored by git.
 OUTPUT_FOLDER: str = path.join(ROOT_PATH, "output_folder")
 
-ASSETS_FOLDER: str = path.join(ROOT_PATH, "Algorithms", "Assets")
-
-# Folder where Algorithms configuration is stored.
-
-CONFIGS_FOLDER: str = path.join(ROOT_PATH, "Algorithms", "Configs")
--- lox_services/finance/billing/constants.py
+++ lox_services/finance/billing/constants.py
@@ -6,6 +6,10 @@ BILLING_ASSETS_FOLDER = join(dirname(__file__), "assets")
 #10 € minimum to invoice a customer, otherwise customer is skipped this month
 MIN_INVOICING_AMOUNT = 10
 
+ACCOUNTANT_EMAIL = "syfers.6285@mailsyfers.nl"
+LOX_CC_EMAIL = "dylan.hirsch@loxsolution.com"
+LOX_FR_CC_EMAIL = "isis.fordant@loxsolution.com"
+
 MANDATORY_BILLING_FIELDS = [
     "company",
     "country",
