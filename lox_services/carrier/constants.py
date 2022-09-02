69e2d7f3f1803715384199f0a0211f568f258294
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-09-02 14:09:43 +0200

    use no sandbox as first option

--- lox_services/carrier/constants.py
+++ lox_services/carrier/constants.py
@@ -2,8 +2,8 @@ from copy import deepcopy
 from selenium.webdriver.chrome.options import Options
 
 _options = Options()
-_options.add_argument("--start-maximized")
 _options.add_argument("--no-sandbox")
 _options.add_argument("--disable-dev-shm-usage")
+_options.add_argument("--start-maximized")
 
 CHROME_OPTIONS = deepcopy(_options)
