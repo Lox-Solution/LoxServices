fd64e19d5217ff6b3a36e37039d205bb90f9494d
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-09-02 12:04:06 +0200

    add default path

--- lox_services/config/paths.py
+++ lox_services/config/paths.py
@@ -3,7 +3,7 @@ from os import getenv, path
 
 # To find easily the root of the project. 
 # Should be like this: "local_absolute_path/Main-Algorithms/Algorithms" 
-ROOT_PATH: str = getenv("PYTHONPATH")
+ROOT_PATH: str = getenv("PYTHONPATH", "/")
 
 # Folder where results will be stored. These folders are ignored by git.
 OUTPUT_FOLDER: str = path.join(ROOT_PATH, "output_folder")
