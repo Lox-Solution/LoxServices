3724af507b8b7945d1723e35969bae4407f46aa5
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-09-02 16:56:24 +0200

    try to reduce memory usage of chromium

--- lox_services/carrier/constants.py
+++ lox_services/carrier/constants.py
@@ -1,9 +1,17 @@
-from copy import deepcopy
 from selenium.webdriver.chrome.options import Options
 
-_options = Options()
-_options.add_argument("--no-sandbox")
-_options.add_argument("--disable-dev-shm-usage")
-_options.add_argument("--start-maximized")
+def get_chromedriver_options():
+    """Returns the options for the chromedriver"""
+    _options = Options()
+    _options.add_argument("--no-sandbox")
+    _options.add_argument("--disable-dev-shm-usage")
+    _options.add_argument("--start-maximized")
 
-CHROME_OPTIONS = deepcopy(_options)
+    chrome_prefs = {}
+    _options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+
+    return _options
+
+
+CHROME_OPTIONS = get_chromedriver_options()
