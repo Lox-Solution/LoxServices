966af3eeb19c7d2e6e948f9ca545e9aeaec1aa1b
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-09-02 12:10:05 +0200

    remove headless because not working

--- lox_services/carrier/constants.py
+++ lox_services/carrier/constants.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 from selenium.webdriver.chrome.options import Options
 
 _options = Options()
-_options.add_argument("--headless")
+# _options.add_argument("--headless")
 _options.add_argument("--start-maximized")
 _options.add_argument("--no-sandbox")
 
