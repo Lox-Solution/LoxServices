f48d2315f25bd02c4699c799d043d93b440ba9e9
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-09-07 15:54:00 +0200

    disable gpu

--- lox_services/carrier/constants.py
+++ lox_services/carrier/constants.py
@@ -2,17 +2,15 @@ from selenium.webdriver.chrome.options import Options
 
 def get_chromedriver_options():
     """Returns the options for the chromedriver"""
-    _options = Options()
-    _options.add_argument("--headless")
-    _options.add_argument("--no-sandbox")
-    _options.add_argument("--disable-dev-shm-usage")
-    _options.add_argument("--start-maximized")
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument('--disable-gpu')
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--start-maximized")
 
-    chrome_prefs = {}
-    _options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
-
-    return _options
+    return options
 
 
 CHROME_OPTIONS = get_chromedriver_options()
