7b8e67c94ce99a17c9ca82dfc8bad4b5f50a93ee
Alexandre Girbal <79905529+alexandregirbal-loxsolution@users.noreply.github.com>
2022-09-27 15:43:39 +0200

    Update billing translation to use Mollie payment (#11)
    * add mollie client in requirements and setup
    
    * add translations
    
    * Revert "add mollie client in requirements and setup"
    
    This reverts commit 11cac8b5720d01619fff43c8023f457af3671039.
    
    * Update version and changelog
    
    * add main algo changes

--- lox_services/persistence/database/schemas/LoxData/DueInvoices.sql
+++ lox_services/persistence/database/schemas/LoxData/DueInvoices.sql
@@ -4,6 +4,7 @@ LoxData.DueInvoices
     invoice_date DATE NOT NULL, --REQUIRED
     status STRING NOT NULL, --REQUIRED
     invoice_number STRING NOT NULL, --REQUIRED
+    mollie_payment_id STRING,
     insert_datetime DATETIME NOT NULL, --REQUIRED
     update_datetime DATETIME
 )
