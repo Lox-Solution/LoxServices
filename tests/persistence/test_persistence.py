0abe1c8f0b88ef545400b52ea79bd1955eb3d067
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-10-26 16:45:36 +0200

    add persistence tests

---
+++ tests/persistence/test_persistence.py
@@ -0,0 +1,31 @@
+import unittest
+import pandas as pd
+import numpy as np
+from pandas.testing import assert_frame_equal
+
+from lox_services.persistence.database.utils import (
+    generate_id, 
+    format_datetime, 
+    format_time, 
+    replace_nan_with_none_in_dataframe, 
+    equal_condition_handle_none_value)
+
+
+label = None
+account = '123456'
+mock_df = pd.DataFrame(np.array([['1', np.NaN, None], [ '5', '6', np.NaN], [ '8', '9', np.NaN]]), columns=['a', 'b', 'c'])
+df = replace_nan_with_none_in_dataframe(mock_df)
+mock_df_out = pd.DataFrame(np.array([['1', None, None], [ '5', '6', None], [ '8', '9', None]]), columns=['a', 'b', 'c'])
+
+class Test_database_functions(unittest.TestCase):
+    
+    def test_utils_functions(self):
+        self.assertEqual(generate_id(['invoice', 'date', 'amount']),'invoice_date_amount')
+        self.assertRaises(Exception, generate_id, [])
+        self.assertEqual(format_time('21:00'),'21:00:00')
+        self.assertEqual(format_datetime('12-12-2020', '12:00'),'12-12-2020T12:00')
+        self.assertEqual(equal_condition_handle_none_value("label", label), 'label is null')
+        self.assertEqual(equal_condition_handle_none_value("account", account), 'account = "123456"')
+        assert_frame_equal(mock_df,mock_df_out)
+
+        
