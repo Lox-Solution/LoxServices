40989628e97c823f48ba6d5ac371e519d8ca3511
Natasa Zekic <natasa.zekic@loxsolution.com>
2022-10-26 16:46:47 +0200

    add development environment just because of imports

--- activate.sh
+++ activate.sh
@@ -5,4 +5,8 @@ source venv/bin/activate
 
 pip install -r requirements.txt # > /dev/null
 
+export ENVIRONMENT='development'
+echo -e "--- using DEVELOLPMENT environment${NC}\n"
+
 export PYTHONPATH=$(pwd)
+echo -e "--- PYTHONPATH: ${NC} $PYTHONPATH ---"
