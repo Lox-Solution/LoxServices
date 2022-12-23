7bb16ae248725edb14c957670513d99359a25fba
Alexandre Girbal <alexandre.girbal@loxsolution.com>
2022-12-23 10:00:37 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='lox_services',
-    version='0.1.8',
+    version='0.1.9',
     author='Lox Solution',
     author_email='natasa.zekic@loxsolution.com',
     description='A package with Lox services',
