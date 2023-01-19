cf26ecd06a6717d6eacd8831e4e74800c3c2966b
Vladimir <vladimir.vilimaitis@loxsolution.com>
2023-01-19 10:29:11 +0100

    Added fake name generator utility function.

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='lox_services',
-    version='0.1.12',
+    version='0.1.13',
     author='Lox Solution',
     author_email='natasa.zekic@loxsolution.com',
     description='A package with Lox services',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Lox-Solution/LoxServices',
-    project_urls = {
+    project_urls={
         "Bug Tracker": "https://github.com/Lox-Solution/LoxServices/issues"
     },
     license='MIT',
     packages=find_packages(),
-    include_package_data = True,
-    install_requires = [
+    include_package_data=True,
+    install_requires=[
         "google-cloud >= 0.34.0",
         "google-cloud-storage >= 1.38.0",
         "google-cloud-bigquery >= 2.10.0",
@@ -31,7 +31,7 @@ setup(
         "selenium >= 4.0.0",
         "undetected-chromedriver >= 3.0.3",
         "selenium-wire >= 4.5.4",
-        "xvfbwrapper >= 0.2.9", 
+        "xvfbwrapper >= 0.2.9",
         "numpy >= 1.22.4",
         "pandas >= 1.4.2",
         "tabula-py == 2.2.0",
@@ -40,5 +40,6 @@ setup(
         "cryptography >= 37.0.2",
         "paramiko >= 2.11.0",
         "PyVirtualDisplay >= 3.0",
+        "mimesis >= 7.0.0"
     ]
 )
