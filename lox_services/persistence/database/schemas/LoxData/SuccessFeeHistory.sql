088a7dce42f4c0d42b1f5da82659087329874c0b
Alexandre Girbal <79905529+alexandregirbal-loxsolution@users.noreply.github.com>
2023-01-31 09:11:36 +0000

    add SuccessFeeHistory table in LoxData schemas (#27)
    add successfeehistory table in schema

---
+++ lox_services/persistence/database/schemas/LoxData/SuccessFeeHistory.sql
@@ -0,0 +1,8 @@
+LoxData.SuccessFeeHistory
+(
+    company STRING NOT NULL,
+    success_fee_refund INT64 NOT NULL,
+    inserter_email STRING NOT NULL,
+    insert_datetime DATETIME NOT NULL,
+    update_datetime DATETIME
+)
