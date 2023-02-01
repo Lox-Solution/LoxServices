fdd8a998be9ab994d2b01f8d3ef3de93053f56e9
mathisbrrt <79903652+mathisbrrt@users.noreply.github.com>
2023-02-01 11:58:02 +0100

    allow usage of parameterized query (#26)
    * allow use of parameterized queries
    
    * Fix issue on Sequence parameters
    
    * create enum with parameter types
    
    * get the value of BQParameterType
    
    * typos
    
    * Update lox_services/persistence/database/query_handlers.py
    
    * Update lox_services/persistence/database/query_handlers.py
    
    * Update lox_services/persistence/database/query_handlers.py
    
    * Update lox_services/persistence/database/query_handlers.py
    
    * fix doc
    
    ---------
    
    Co-authored-by: natasazekic-loxsolution <106743462+natasazekic-loxsolution@users.noreply.github.com>

--- lox_services/persistence/database/query_handlers.py
+++ lox_services/persistence/database/query_handlers.py
@@ -2,26 +2,36 @@
 import os
 import re
 import time
+from typing import Any, Optional, Tuple
+from collections.abc import Sequence
 
-from google.cloud.bigquery import Client
+from google.cloud.bigquery import Client, QueryJobConfig, ScalarQueryParameter, ArrayQueryParameter
 from google.cloud.bigquery.job import QueryJob
 from pandas import DataFrame
 
 from lox_services.persistence.config import SERVICE_ACCOUNT_PATH
 from lox_services.persistence.database.exceptions import BadQueryTypeException, MissingUpdateDatetimeException
 import lox_services.utils.general_python as gpy
-from lox_services.utils.enums import Colors
+from lox_services.utils.enums import BQParameterType, Colors
 from lox_services.utils.metadata import get_function_callers
 
-def raw_query(query: str, print_query: bool = True) -> QueryJob:
+def raw_query(
+    query: str, 
+    *, 
+    parameters:  Optional[Sequence[Tuple[str, BQParameterType, Any]]] = None,
+    print_query: bool = True
+    ) -> QueryJob:
+
     """Excecutes a query with Google BigQuery, without any checks.
         Adds some metadata at the beginning of the query.
         ## Arguments
         - `query`: String representation of the query to be executed.
         - `print_query`: Tells whether or not to print the query before executing it.
+        - `parameters`: List of parameters used to avoid SQL injection
 
         ## Example
             >>> raw_query("SELECT * FROM InvoicesData.Refunds LIMIT 10")
+            >>> select ("SELECT * FROM InvoicesData.Refunds where carrier=@carrier LIMIT 10", parameters = [("carrier", BQParameterType.STRING, "UPS")])
 
         ## Return
         The the query job from Google BigQuery. It needs some actions to be rendered as a dataframe (.result().to_dataframe()).
@@ -47,19 +57,30 @@ def raw_query(query: str, print_query: bool = True) -> QueryJob:
         ))
 
     bigquery_client = Client()
-    query_job = bigquery_client.query(query)
+    
+    if parameters: 
+        parameters = QueryJobConfig(
+        query_parameters=[
+            ArrayQueryParameter(parameter[0], parameter[1].value, parameter[2]) 
+            if (isinstance(parameter[2], Sequence) and not isinstance(parameter[2], str)) 
+            else ScalarQueryParameter(parameter[0], parameter[1].value, parameter[2]) for parameter in parameters
+        ])
+    
+    query_job = bigquery_client.query(query, job_config=parameters)
     query_job.result()
     return query_job
 
 
-def select(query: str, print_query: bool = True) -> DataFrame:
+def select(query: str, print_query: bool = True, *, parameters:  Optional[Sequence[Tuple[str, BQParameterType, Any]]] = None) -> DataFrame:
     """Checks if the query begings with a SELECT statement. If so the query is being executed.
         ## Arguments
         - `query`: String representation of the query to be executed.
         - `print_query`: Tells whether or not to print the query before executing it.
+        - `parameters`: List of parameters used to avoid SQL injection
 
         ## Example
-            >>> select("SELECT * FROM InvoicesData.Refunds LIMIT 10")
+            >>> select("SELECT * FROM InvoicesData.Refunds where carrier="UPS" LIMIT 10")
+            >>> select ("SELECT * FROM InvoicesData.Refunds where carrier=@carrier LIMIT 10", parameters = [("carrier", BQParameterType.STRING, "UPS")])
 
         ## Return
         The result of the select query as a dataframe.
@@ -67,17 +88,20 @@ def select(query: str, print_query: bool = True) -> DataFrame:
     if not (query.lstrip().startswith("SELECT") or query.lstrip().startswith("WITH")) :
         raise BadQueryTypeException('SELECT or WITH')
 
-    return raw_query(query, print_query).result().to_dataframe()
+    return raw_query(query, print_query = print_query, parameters = parameters).result().to_dataframe()
 
 
-def update(query: str, print_query: bool = True) -> int:
+def update(query: str, print_query: bool = True, *, parameters:  Optional[Sequence[Tuple[str, BQParameterType, Any]]] = None) -> DataFrame:
     """Checks if the query begings with a UPDATE statement. If so the query is being executed.
         ## Arguments
         - `query`: String representation of the query to be executed.
         - `print_query`: Tells whether or not to print the query before executing it.
+        - `parameters`: List of parameters used to avoid SQL injection
 
         ## Example
             >>> update("UPDATE InvoicesData.Refunds SET state='Test' WHERE company='Test'")
+            >>> update("UPDATE InvoicesData.Refunds SET state='Test' where company=@company", parameters = [("company", BQParameterType.STRING, "Test")])
+
 
         ## Return
         The result of the update query is a number of affected rows.
@@ -95,7 +119,7 @@ def update(query: str, print_query: bool = True) -> int:
     result=None
     while not query_success:
         try:
-            result=raw_query(query, print_query)
+            result=raw_query(query, print_query = print_query, parameters=parameters)
             print("Rows affected:", result.num_dml_affected_rows)
             result_returned = result.num_dml_affected_rows
             query_success=True
@@ -115,25 +139,25 @@ def update(query: str, print_query: bool = True) -> int:
         raise Exception("Error processing update: ", query)
 
 
-def delete(query: str, print_query: bool = True) -> DataFrame:
+def delete(query: str, print_query: bool = True, *, parameters:  Optional[Sequence[Tuple[str, BQParameterType, Any]]] = None) -> DataFrame:
     """Checks if the query begings with a DELETE statement. If so the query is being executed.
     
         ## Arguments
         
         - `query`: String representation of the query to be executed.
-        
         - `print_query`: Tells whether or not to print the query before executing it.
+        - `parameters`: List of parameters used to avoid SQL injection
         
         ## Example
         
-            >>> select("DELETE InvoicesData.Refunds WHERE tracking_number = '1467BDYV'")
-
+            >>> delete("DELETE FROM InvoicesData.Refunds WHERE tracking_number = '1467BDYV'")
+            >>> delete("DELETE FROM InvoicesData.Refunds WHERE tracking_number=@tracking_number", parameters = [("tracking_number", BQParameterType.STRING, "1467BDYV")])
         ## Return
         
-        The result of the select query as a dataframe.
+        The result of the query is the number of affected rows.
     """
     if not query.lstrip().startswith("DELETE"):
         raise BadQueryTypeException('DELETE')
     
-    result = raw_query(query, print_query)                
+    result = raw_query(query, print_query = print_query, parameters = parameters)                
     print("Rows deleted:", result.num_dml_affected_rows)
