e4babaea5de4d9a2a2fd9a633e7b3c570142bdb8
VovaViliLox <122106450+VovaViliLox@users.noreply.github.com>
2023-04-12 13:54:59 +0200

    Fixed floating point conversion bug. (#72)
    * version update 0.1.16
    
    * Use iloc instead of at in case if there is no index
    
    * Fixed floating point conversion bug.
    
    * Keep the code DRY by extracting repetitions into a function
    
    * Added a function to validate country codes. In the future, it can be adapted for pandera or Great Expectations.
    
    * Add an option to handle sequence of columns. Update the unit test accordingly.
    
    * A more descriptive function name. Added handling to both postal code sender and receiver.
    
    * Responded to Melvil's comments
    
    * Fixed typo
    
    ---------
    
    Co-authored-by: Natasa Zekic <natasa.zekic@loxsolution.com>
    Co-authored-by: Melvil Donnart <melvil.donnart@loxsolution.com>

--- tests/persistence/database.py
+++ tests/persistence/database.py
@@ -7,7 +7,10 @@ import pandas as pd
 from google.cloud.bigquery import Client, DatasetReference
 
 from lox_services.persistence.config import SERVICE_ACCOUNT_PATH
-from lox_services.persistence.database.utils import make_temporary_table
+from lox_services.persistence.database.utils import (
+    make_temporary_table,
+    validate_country_code,
+)
 
 
 class TestDatabaseFunctions(unittest.TestCase):
@@ -30,3 +33,66 @@ class TestDatabaseFunctions(unittest.TestCase):
             (datetime.now(timezone.utc) + timedelta(hours=1)) - table_ref.expires,
             timedelta(seconds=1),
         )
+
+
+class TestValidateISO31662(unittest.TestCase):
+    def setUp(self):
+        # Create a DataFrame for testing
+        self.df = pd.DataFrame(
+            {
+                "country_code_1": [
+                    "US",
+                    "CA",
+                    "MX",
+                    "GB",
+                    "FR",
+                    "JP",
+                    "CN",
+                    "IN",
+                    "BR",
+                    "AU",
+                ],
+                "country_code_2": [
+                    "GB",
+                    "FR",
+                    "JP",
+                    "CN",
+                    "IN",
+                    "BR",
+                    "AU",
+                    "US",
+                    "CA",
+                    "MX",
+                ],
+            }
+        )
+
+    def test_validate_country_code_with_valid_codes(self):
+        # Test the function with valid ISO-3166-2 country codes
+        self.assertIsNone(validate_country_code(self.df, "country_code_1"))
+        self.assertIsNone(
+            validate_country_code(self.df, ["country_code_1", "country_code_2"])
+        )
+
+    def test_validate_country_code_with_invalid_codes(self):
+        # Test the function with invalid ISO-3166-2 country codes
+        self.df.loc[3, "country_code_1"] = "GB1"
+        self.df.loc[2, "country_code_2"] = "CA1"
+        with self.assertRaises(ValueError):
+            validate_country_code(self.df, "country_code_1")
+        with self.assertRaises(ValueError):
+            validate_country_code(self.df, ["country_code_1", "country_code_2"])
+
+    def test_validate_country_code_with_missing_values(self):
+        # Test the function with missing values in the country code column
+        self.df.loc[4, "country_code_1"] = pd.NA
+        self.df.loc[2, "country_code_2"] = pd.NA
+        self.assertIsNone(validate_country_code(self.df, "country_code_1"))
+        self.assertIsNone(
+            validate_country_code(self.df, ["country_code_1", "country_code_2"])
+        )
+
+    def test_validate_country_code_with_nonexistent_country_code_column(self):
+        # Test the function with a non-existent country code column
+        with self.assertRaises(KeyError):
+            validate_country_code(self.df, "invalid_column_name")
