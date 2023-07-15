eb4fec4ae986073ce2699d77dad6bc77cf948c0e
Melvil Donnart <melvil.donnart@loxsolution.com>
2023-07-15 12:20:19 +0200

    Add invalid base 64 pdf assert

--- tests/pdf/test_writer.py
+++ tests/pdf/test_writer.py
@@ -1,10 +1,13 @@
-
 import os
 import unittest
 import pandas as pd
 from base64 import b64encode
 from tempfile import NamedTemporaryFile
-from lox_services.pdf.writer import save_pdf_from_base_64, dataframe_to_html_without_pandas_style, generate_pdf_file_from_dataframe
+from lox_services.pdf.writer import (
+    save_pdf_from_base_64,
+    dataframe_to_html_without_pandas_style,
+    generate_pdf_file_from_dataframe,
+)
 from pathlib import Path
 
 
@@ -12,15 +15,27 @@ class Test_writer_pdf_functions(unittest.TestCase):
     """
     A unit test class for the save_pdf_from_base_64 function.
     """
+
     def setUp(self):
         self.base_dir = os.path.join(
             os.path.dirname(__file__),
             "Files",
         )
-        
+
         if not os.path.exists(self.base_dir):
             os.mkdir(self.base_dir)
-            
+
+    def test_dataframe_to_html_without_pandas_style(self):
+        html = dataframe_to_html_without_pandas_style(pd.DataFrame())
+        self.assertIn("<tbody>", html)
+
+    def test_generate_pdf_file_dataframe(self):
+        # Case 1: From a dataframe
+        pdf_path = os.path.join(self.base_dir, "test_df.pdf")
+        generate_pdf_file_from_dataframe(pd.DataFrame(), pdf_path, False)
+
+        self.assertTrue(os.path.exists(pdf_path))
+
     def test_save_pdf_from_base_64(self):
         # Case 1: Create a temporary file to save the PDF
         pdf_data = b"%PDFDummyPDFContent"
@@ -41,23 +56,23 @@ class Test_writer_pdf_functions(unittest.TestCase):
         Path(temp_file_path).unlink()
 
         # Case 2: Raise an exception if the base64 data is not valid
-        with self.assertRaises(ValueError):
-            save_pdf_from_base_64("Not a base64 string", temp_file_path)
-    
-    def test_dataframe_to_html_without_pandas_style(self):
-        
-        html = dataframe_to_html_without_pandas_style(pd.DataFrame())
-        self.assertIn("<tbody>", html)
-        
-    def test_generate_pdf_file_dataframe(self):
-        
-        # Case 1: From a dataframe
-        pdf_path = os.path.join(self.base_dir, "test_df.pdf")
-        generate_pdf_file_from_dataframe(pd.DataFrame(), pdf_path, False)
-        
-        self.assertTrue(os.path.exists(pdf_path))
-        
-        
+        # Create an invalid PDF Base64 string (missing PDF signature)
+        invalid_pdf_base64 = b64encode(b"Invalid PDF data").decode("utf-8")
+
+        # Create a temporary file to save the PDF
+        with NamedTemporaryFile(delete=False) as temp_file:
+            file_path = temp_file.name
+
+        # Call the function being tested and expect a ValueError to be raised
+        with self.assertRaises(ValueError) as context:
+            save_pdf_from_base_64(invalid_pdf_base64, file_path)
+
+        # Verify that the expected error message is raised
+        self.assertEqual(str(context.exception), "Missing the PDF file signature")
+
+        # Clean up the temporary file
+        Path(file_path).unlink()
+
 
 if __name__ == "__main__":
     unittest.main()
