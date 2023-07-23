5476e9496274fd4a8474ba50e9b7c4528538088a
Melvil Donnart <melvil.donnart@loxsolution.com>
2023-07-23 17:25:07 +0200

    Slack - test both prod and dev by mocking env and slack channel

--- tests/slack/test_send_message.py
+++ tests/slack/test_send_message.py
@@ -1,11 +1,30 @@
 import unittest
+from lox_services.config.env_variables import get_env_variable
 from lox_services.slack.send_message import send_messages, tag_someone
 from lox_services.utils.enums import SlackMemberID
+from mock import patch
 
 
 class TestSlackSendMessages(unittest.TestCase):
     def test_send_messages(self):
-        send_messages("test_message", "test_title")
+        # Check that the function works using a test slack channel
+        with patch.dict(
+            "os.environ",
+            {
+                "ENVIRONMENT": "production",
+                "RUNS_REPORTS": get_env_variable("RUNS_REPORTS_TEST"),
+            },
+        ):
+            send_messages("test_message", "test_title")
+
+        with patch.dict(
+            "os.environ",
+            {
+                "ENVIRONMENT": "development",
+                "RUNS_REPORTS_DEV": get_env_variable("RUNS_REPORTS_TEST"),
+            },
+        ):
+            send_messages("test_message", "test_title")
 
     def test_tag_someone(self):
         tag = tag_someone(SlackMemberID.MELVIL)
