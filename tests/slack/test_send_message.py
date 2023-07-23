bdd87a025dfd0fc8953db6dd1af11fa376e9200e
Melvil Donnart <melvil.donnart@loxsolution.com>
2023-07-23 17:38:11 +0200

    Slack: Mock Exception

--- tests/slack/test_send_message.py
+++ tests/slack/test_send_message.py
@@ -6,8 +6,8 @@ from mock import patch
 
 
 class TestSlackSendMessages(unittest.TestCase):
-    def test_send_messages(self):
-        # Check that the function works using a test slack channel
+    def test_send_messages_production(self):
+        # Case 1: Mock production environment
         with patch.dict(
             "os.environ",
             {
@@ -17,6 +17,8 @@ class TestSlackSendMessages(unittest.TestCase):
         ):
             send_messages("test_message", "test_title")
 
+    def test_send_messages_development(self):
+        # Case 2: Mock development environment
         with patch.dict(
             "os.environ",
             {
@@ -25,6 +27,27 @@ class TestSlackSendMessages(unittest.TestCase):
             },
         ):
             send_messages("test_message", "test_title")
+
+    @patch("requests.post")  # Mocking the requests.post function
+    def test_send_messages_development(self, mock_post):
+        # Case 3: Mock Exception
+        with patch.dict(
+            "os.environ",
+            {
+                "ENVIRONMENT": "development",
+                "RUNS_REPORTS_DEV": get_env_variable("RUNS_REPORTS_TEST"),
+            },
+        ):
+            mock_response = mock_post.return_value
+            mock_response.status_code = 500
+            mock_response.text = "Internal Server Error"
+
+            # Call the function under test and expect it to raise an exception
+            with self.assertRaises(Exception) as context:
+                send_messages("Title", "Message")
+
+            # Assert that the exception contains the correct status code and response text
+            self.assertEqual(context.exception.args, (500, "Internal Server Error"))
 
     def test_tag_someone(self):
         tag = tag_someone(SlackMemberID.MELVIL)
