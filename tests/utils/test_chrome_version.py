83260d4e39862261a7fef45561d78d02d2ab4e1f
Filip Nguyen Duc <filip.nguyenduc@loxsolution.com>
2023-07-24 15:40:39 +0200

    Add setUp() and not mocked test

--- tests/utils/test_chrome_version.py
+++ tests/utils/test_chrome_version.py
@@ -1,6 +1,5 @@
 import unittest
 from unittest.mock import MagicMock, patch
-import sys
 
 from lox_services.utils.chrome_version import (
     extract_version_registry,
@@ -11,19 +10,23 @@ from lox_services.utils.chrome_version import (
 
 
 class TestChromeVersion(unittest.TestCase):
-    def test_extract_version_registry(self):
-        output = """
+    def setUp(self):
+        self.dummy_fallback_value = 12
+        self.dummy_version_value = 92
+        self.dummy_version = "92.0.4515.107"
+        self.dummy_full_version = "Google Chrome 92.0.4515.107"
+        self.incorrect_version = "91,0,4472,164"
+        self.registry_output = """
         Some other registry values...
 
         DisplayVersion    REG_SZ    92.0.4515.107
         """
-        version = extract_version_registry(output)
-        self.assertEqual(version, "92.0.4515.107")
+
+    def test_extract_version_registry(self):
+        version = extract_version_registry(self.registry_output)
+        self.assertEqual(version, self.dummy_version)
 
     def test_extract_version_folder(self):
-        # You can create some test directories and files to simulate the Chrome installation folders
-        # For simplicity, let's just set one version here:
-        chrome_version = "91.0.4472.164"
         with patch("os.scandir") as mock_scandir:
             with patch("os.path.isdir") as mock_isdir:
                 mock_isdir.return_value = True
@@ -33,16 +36,15 @@ class TestChromeVersion(unittest.TestCase):
                         (object,),
                         {
                             "is_dir": lambda x: True,
-                            "path": f"/path/to/Chrome/{chrome_version}",
+                            "path": f"/path/to/Chrome/{self.dummy_version}",
                         },
                     )()
                 ]
 
                 version = extract_version_folder()
-                self.assertEqual(version, chrome_version)
+                self.assertEqual(version, self.dummy_version)
 
     def test_extract_version_folder_not_found(self):
-        chrome_version = "91,0,4472,164"
         with patch("os.scandir") as mock_scandir:
             with patch("os.path.isdir") as mock_isdir:
                 mock_isdir.return_value = True
@@ -52,7 +54,7 @@ class TestChromeVersion(unittest.TestCase):
                         (object,),
                         {
                             "is_dir": lambda x: True,
-                            "path": f"/path/to/Chrome/{chrome_version}",
+                            "path": f"/path/to/Chrome/{self.incorrect_version}",
                         },
                     )()
                 ]
@@ -64,6 +66,12 @@ class TestChromeVersion(unittest.TestCase):
     ### Test get_chrome_version
     ###
 
+    # Real run (not mocked)
+    def test_get_chrome_version_not_mocked(self):
+        version = get_chrome_version(self.dummy_fallback_value)
+        self.assertIsInstance(version, int)
+        self.assertNotEqual(version, self.dummy_fallback_value)
+
     # Linux
     def test_get_chrome_version_linux(self):
         with patch(
@@ -74,10 +82,10 @@ class TestChromeVersion(unittest.TestCase):
                 mock_exists.return_value = True
                 with patch("os.popen") as mock_popen:
                     mock_popen.return_value = MagicMock(
-                        read=MagicMock(return_value="Google Chrome 92.0.4515.107")
+                        read=MagicMock(return_value=self.dummy_full_version)
                     )
-                    version = get_chrome_version(12)
-                    self.assertEqual(version, 92)
+                    version = get_chrome_version(self.dummy_fallback_value)
+                    self.assertEqual(version, self.dummy_version_value)
 
     # Mac
     def test_get_chrome_version_mac(self):
@@ -87,10 +95,10 @@ class TestChromeVersion(unittest.TestCase):
             mock_get_platform.return_value = "mac"
             with patch("os.popen") as mock_popen:
                 mock_popen.return_value = MagicMock(
-                    read=MagicMock(return_value="Google Chrome 92.0.4515.107")
+                    read=MagicMock(return_value=self.dummy_full_version)
                 )
-                version = get_chrome_version(12)
-                self.assertEqual(version, 92)
+                version = get_chrome_version(self.dummy_fallback_value)
+                self.assertEqual(version, self.dummy_version_value)
 
     # Windows
     def test_get_chrome_version_windows_registry(self):
@@ -103,9 +111,9 @@ class TestChromeVersion(unittest.TestCase):
                 with patch(
                     "lox_services.utils.chrome_version.extract_version_registry"
                 ) as mock_extract_version_registry:
-                    mock_extract_version_registry.return_value = "92.0.4515.107"
-                    version = get_chrome_version(12)
-                    self.assertEqual(version, 92)
+                    mock_extract_version_registry.return_value = self.dummy_version
+                    version = get_chrome_version(self.dummy_fallback_value)
+                    self.assertEqual(version, self.dummy_version_value)
 
     def test_get_chrome_version_windows_folder(self):
         with patch(
@@ -121,9 +129,9 @@ class TestChromeVersion(unittest.TestCase):
                     with patch(
                         "lox_services.utils.chrome_version.extract_version_folder"
                     ) as mock_extract_version_folder:
-                        mock_extract_version_folder.return_value = "92.0.4515.107"
-                        version = get_chrome_version(12)
-                        self.assertEqual(version, 92)
+                        mock_extract_version_folder.return_value = self.dummy_version
+                        version = get_chrome_version(self.dummy_fallback_value)
+                        self.assertEqual(version, self.dummy_version_value)
 
     def test_get_chrome_version_windows_folder(self):
         with patch(
@@ -140,8 +148,8 @@ class TestChromeVersion(unittest.TestCase):
                         "lox_services.utils.chrome_version.extract_version_folder"
                     ) as mock_extract_version_folder:
                         mock_extract_version_folder.side_effect = RuntimeError
-                        version = get_chrome_version(12)
-                        self.assertEqual(version, 12)
+                        version = get_chrome_version(self.dummy_fallback_value)
+                        self.assertEqual(version, self.dummy_fallback_value)
 
     @patch("sys.platform", "linux")
     def test_linux_platform(self):
