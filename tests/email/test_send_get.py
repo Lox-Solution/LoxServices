f4d8fbf61998cf8389f3a1c5054afc0b449df07b
Filip Nguyen Duc <filip.nguyenduc@loxsolution.com>
2023-07-28 16:54:16 +0200

    Add email_send_get

--- tests/email/test_send_get.py
+++ tests/email/test_send_get.py
@@ -1,11 +1,15 @@
 import os
+import random
 import unittest
 from lox_services.email.send import send_emails_from_loxsolution_account
-from lox_services.config.paths import OUTPUT_FOLDER
+from lox_services.email.get import get_emails, download_attachments
+from tests import OUTPUT_FOLDER
+from email.message import Message
 
 
 class TestSend(unittest.TestCase):
     def setUp(self):
+        self.index = random.randint(0, 1000000)
         self.sender_email = "loxteam@loxsolution.com"
         self.incorrect_sender_email = "dummy@dummy.com"
         self.receiver_emails = ["loxteam@loxsolution.com"]
@@ -14,19 +18,29 @@ class TestSend(unittest.TestCase):
         self.subject = "Unit Testing LoxServices"
         self.content = "This is a test email sent from the LoxServices unit tests."
         self.pdf_path = os.path.join(OUTPUT_FOLDER, "XXXXXDXXXXXXXX.pdf")
+        self.expected_subject = f"{self.subject} - {self.index}"
 
     def test_send_emails(self):
-        # Run the function with the test data
-        with self.assertRaises(None):
-            send_emails_from_loxsolution_account(
-                sender_email_address=self.sender_email,
-                receiver_email_addresses=self.receiver_emails,
-                cc_email_addresses=self.cc_email_addresses,
-                bcc_email_addresses=self.bcc_email_addresses,
-                subject=self.subject,
-                content=self.content,
-                attachments=[self.pdf_path],
-            )
+        send_emails_from_loxsolution_account(
+            sender_email_address=self.sender_email,
+            receiver_email_addresses=self.receiver_emails,
+            cc_email_addresses=self.cc_email_addresses,
+            bcc_email_addresses=self.bcc_email_addresses,
+            subject=f"{self.subject} - {self.index}",
+            content=self.content,
+            attachments=[self.pdf_path],
+        )
+
+        # Call the function with mock data
+        emails = get_emails("Unittest", days=1, search={"subject": self.subject})
+
+        # Assert the results
+        self.assertEqual(len(emails), 1)
+        self.assertIsInstance(emails[0], Message)
+        self.assertEqual(emails[0]["Subject"], self.expected_subject)
+
+        file_names = download_attachments(emails[0], self.output_folder)
+        self.assertEqual(file_names[0], self.pdf_name)
 
     def test_invalid_sender_email(self):
         # Run the function with the test data and expect a ValueError
