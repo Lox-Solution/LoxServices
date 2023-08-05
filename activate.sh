aedc89279cf28ec58075dd0d5adb3849ba614b53
Melvil Donnart <melvil.donnart@loxsolution.com>
2023-08-05 13:14:21 +0200

    fix typo

--- activate.sh
+++ activate.sh
@@ -6,7 +6,7 @@ source venv/bin/activate
 pip install -r requirements.txt # > /dev/null
 
 export ENVIRONMENT='development'
-echo -e "--- using DEVELOLPMENT environment${NC}\n"
+echo -e "--- using DEVELOPMENT environment${NC}\n"
 
 export PYTHONPATH=$(pwd)
 echo -e "--- PYTHONPATH: ${NC} $PYTHONPATH ---"
