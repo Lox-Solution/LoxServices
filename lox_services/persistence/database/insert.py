d66cd9624977b1e987e4ebab46a5857e21ea2c15
melvil-donnart <82094821+melvil-donnart@users.noreply.github.com>
2023-08-07 11:40:09 +0200

    Data Needed DAG crashes due to wrong requirement (#101)
    * Only raise value if invoice_url is present
    
    * Two columns are also needed as used in the function

--- lox_services/persistence/database/insert.py
+++ lox_services/persistence/database/insert.py
@@ -431,16 +431,20 @@ def client_invoice_data_quality_check(dataframe: pd.DataFrame) -> pd.DataFrame:
             "tracking_number",
             "data_source",
             "is_original_invoice",
+            "quantity",
+            "net_amount",
         }.difference(set(dataframe.columns))
     )
     if missing_columns:
         raise MissingColumnsException(", ".join(missing_columns))
-    # Check value invoice_url
-    for invoice_url in dataframe["invoice_url"].to_list():
-        if not pd.isna(invoice_url) and not re.match(
-            r"(https://storage.cloud.google.com).*", invoice_url
-        ):
-            raise ValueError(f"invoice_url has to start by {INVOICE_BASE_URL}")
+
+    if "invoice_url" in dataframe.columns:
+        # Check value invoice_url
+        for invoice_url in dataframe["invoice_url"].to_list():
+            if not pd.isna(invoice_url) and not re.match(
+                r"(https://storage.cloud.google.com).*", invoice_url
+            ):
+                raise ValueError(f"invoice_url has to start by {INVOICE_BASE_URL}")
 
     # Check value is_original_invoice
     is_original_invoice_wrong_values = dataframe[
