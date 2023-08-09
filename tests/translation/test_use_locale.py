b61dab03f00d6ed81a23a6e495a8605be170d224
melvil-donnart <82094821+melvil-donnart@users.noreply.github.com>
2023-08-09 14:21:31 +0200

    Test working locally but not on GA - test with the new docker image (#103)
    * Test working locally but not on GA - test with the new docker image
    
    * test use_locale unsuccessful

--- tests/translation/test_use_locale.py
+++ tests/translation/test_use_locale.py
@@ -15,6 +15,7 @@ class TestLocaleFunctions(unittest.TestCase):
     def setUp(self):
         self.right_language_code = "ru_RU"
         self.right_encoding = "utf8"
+        self.right_encoding_uppercase = "UTF-8"
         self.wrong_language_code = "wrong_WRONG"
 
     def test_get_all_locales(self):
@@ -25,40 +26,51 @@ class TestLocaleFunctions(unittest.TestCase):
         self.assertIsInstance(result, dict)
         self.assertTrue(len(result) > 0)
 
-    # TODO: Need permission to download locale, Github Action is not working because of that.
-    # def test_download_locale(self):
-    #     # Test 1: language_code is not downloadble
-    #     with self.assertRaises(Exception):
-    #         download_locale(self.wrong_language_code, self.right_encoding)
+    def test_download_locale(self):
+        # Test 1: language_code is not downloadble
+        with self.assertRaises(Exception):
+            download_locale(self.wrong_language_code, self.right_encoding)
 
-    #     # Test 2: language_code is downloadble
-    #     download_locale(self.right_language_code, self.right_encoding)
+        # Test 2: language_code is downloadble
+        download_locale(self.right_language_code, self.right_encoding)
 
-    #     # Get the list of installable locales after calling the function
-    #     locales_after = set(locale.locale_alias.keys())
-    #     print(locales_after)
+        # Get the list of installable locales after calling the function
+        locales_after = set(locale.locale_alias.keys())
+        print(locales_after)
 
-    #     # Assertions
-    #     # Check if the new locale is added to the list of installable locales
-    #     self.assertTrue(self.right_language_code.lower() in locales_after)
+        # Assertions
+        # Check if the new locale is added to the list of installable locales
+        self.assertTrue(self.right_language_code.lower() in locales_after)
 
-    # TODO: Need permission to download locale, Github Action is not working because of that.
-    # def test_use_locale(self):
-    #     # Test 1: language_code is not installable
-    #     with self.assertRaises(Exception):
-    #         download_locale(self.wrong_language_code, self.right_encoding)
+    def test_use_locale_successful(self):
+        # Test 1: language_code is not installable
+        with self.assertRaises(Exception):
+            download_locale(self.wrong_language_code, self.right_encoding)
 
-    #     # Test 2: language_code is not installable
-    #     encoding = "UTF-8"
-    #     use_locale(self.right_language_code, encoding)
+        # Test 2: language_code is not installable
+        use_locale(self.right_language_code, self.right_encoding_uppercase)
 
-    #     # Get the current locale setting
-    #     current_locale = locale.setlocale(locale.LC_TIME)
+        # Get the current locale setting
+        current_locale = locale.setlocale(locale.LC_TIME)
 
-    #     # Assertions
-    #     # Check if the current locale is set to the expected language and encoding
-    #     expected_locale = f"{self.right_language_code}.{encoding}"
-    #     self.assertEqual(current_locale, expected_locale)
+        # Assertions
+        # Check if the current locale is set to the expected language and encoding
+        expected_locale = f"{self.right_language_code}.{self.right_encoding_uppercase}"
+        self.assertEqual(current_locale, expected_locale)
+
+    @patch("lox_services.translation.use_locale.download_locale")
+    def test_use_locale_unsuccessful(self, mock_download_locale):
+        # Set up the mock to return without raising an exception
+        mock_download_locale.return_value = None
+
+        # Call the function and expect an exception
+        with self.assertRaises(Exception) as context:
+            use_locale(self.wrong_language_code, self.right_encoding_uppercase)
+
+        self.assertIn(
+            f"Locale '{self.wrong_language_code}' is installed but the current process is not aware of it.",
+            str(context.exception),
+        )
 
 
 if __name__ == "__main__":
