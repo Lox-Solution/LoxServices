7d5ebe84fadc212dff1365b098645385c5eb11c0
Natasa Zekic <natasa.zekic@loxsolution.com>
2023-09-08 15:31:55 +0200

    version update 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="lox_services",
-    version="0.1.29",
+    version="0.2.0",
     author="Lox Solution",
     author_email="natasa.zekic@loxsolution.com",
     description="A package with Lox services",
