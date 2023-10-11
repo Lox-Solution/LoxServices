dac84a8f4f8c55fb580218b42024f5fd34e1e281
Natasa Zekic <natasa.zekic@loxsolution.com>
2023-10-11 16:04:23 +0200

    Update init.sh to handle more python paths

--- init.sh
+++ init.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
 # To run with: sh init.sh
+
 if [ -d "./venv" ]; then
     echo "The Virtual Environment 'venv' has already been created, skipping creation."
-    return 0
+    exit 0
 fi
 
 echo "Creating Virtual Environment as 'venv'..."
-virtualenv -p /usr/bin/python3.8 venv # if this command doesn't work, make sure to install virtualenv on your machine
+
+# A list of potential paths for Python3
+python_paths=("/usr/bin/python3" "/usr/local/bin/python3" "/opt/homebrew/bin/python3")
+
+python_path=""
+
+# Find the Python 3.11 path that exists
+for path in "${python_paths[@]}"; do
+    if [ -x "$path" ]; then
+        version_string="$($path --version 2>&1)"
+        if [[ $version_string == *"Python 3.11"* ]]; then
+            python_path="$path"
+            break
+        fi
+    fi
+done
+
+# Check if a valid Python path was found
+if [ -z "$python_path" ]; then
+    echo "Python3 not found in the predefined paths. Please install Python3 or check your installation."
+    exit 1
+fi
+
+virtualenv -p "$python_path" lox-env # if this command doesn't work, make sure to install virtualenv on your machine
