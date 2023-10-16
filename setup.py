39b6f5a32921b1c9d14f1fec088c81fe5cb5f2b6
Natasa Zekic <natasa.zekic@loxsolution.com>
2023-10-16 10:59:52 +0200

    update python requires limits

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.8, <=3.11",
+    python_requires=">=3.7, <=3.12",
     install_requires=[
         "google-cloud == 0.34.0",
         "google-cloud-storage == 2.11.0",
