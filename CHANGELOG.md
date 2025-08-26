b53b739fb979896c7a5bfbfb4fb97783f1848805
Melvil Donnart <melvil.donnart@loxsolution.com>
2025-08-26 14:29:14 +0200

    change default chrome version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -92,3 +92,4 @@
 1.2.41 Add new datasets
 1.2.42 Add trackingOnly
 1.2.43 Add UserAgent
+1.2.44 Change chrome version
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="lox_services",
-    version="1.2.43",
+    version="1.2.44",
     author="Lox Solution",
     author_email="melvil.donnart@loxsolution.com",
     description="A package with Lox services",
